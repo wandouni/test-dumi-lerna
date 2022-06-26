a1f12e1bae953a89c68e37563d9ad6f769db050a
wandouni <2539419557@qq.com>
2022-06-26 18:06:24 +0800

    feat: add subs

--- packages/system/es/index.js
+++ packages/system/es/index.js
@@ -1,3 +1,6 @@
 export var add = function add(a, b) {
   return a + b;
 };
+export var subs = function subs(a, b) {
+  return a - b;
+};
--- packages/system/lib/index.js
+++ packages/system/lib/index.js
@@ -3,10 +3,16 @@
 Object.defineProperty(exports, '__esModule', {
   value: true,
 });
-exports.add = void 0;
+exports.subs = exports.add = void 0;
 
 var add = function add(a, b) {
   return a + b;
 };
 
 exports.add = add;
+
+var subs = function subs(a, b) {
+  return a - b;
+};
+
+exports.subs = subs;
